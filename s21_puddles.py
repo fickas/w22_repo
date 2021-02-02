32a6db12536b26f758044bf06c0d4e91d5903c47
uo-puddles <59064719+uo-puddles@users.noreply.github.com>
2021-02-02 12:55:07 -0800

    Update s21_puddles.py

--- s21_puddles.py
+++ s21_puddles.py
@@ -1,2 +1,13 @@
+import numpy as np
+from numpy.linalg import norm
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy
+from typing import TypeVar, Callable
+dframe = TypeVar('pd.core.frame.DataFrame')
+narray = TypeVar('numpy.ndarray')
+import math
+import json
+
 def hello():
   return "Welcome to s21_puddles library"
