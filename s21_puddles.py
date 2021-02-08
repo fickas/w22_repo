71bd34dfe51d393f5d8f8aff8eaacfef9fd87438
uo-puddles <59064719+uo-puddles@users.noreply.github.com>
2021-02-08 10:48:54 -0800

    Update s21_puddles.py

--- s21_puddles.py
+++ s21_puddles.py
@@ -11,3 +11,20 @@ import json
 
 def hello():
   return "Welcome to s21_puddles library"
+
+###Week 4
+
+def knn(table, target_list,  k):
+
+  distance_record = []
+  n = len(table)
+
+  for i in range(n):
+    crowd_row = table.loc[i].to_list()
+    crowd_number_list = crowd_row[:-1]  #strip off last column
+    choice = crowd_row[-1]  #grab last column
+    d = euclidean_distance(target_list, crowd_number_list)
+    distance_record += [[d,choice]]
+
+  sorted_record = sorted(distance_record)
+  return sorted_record
