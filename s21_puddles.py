d697178e433bc9b0bd1a78fa516bbdc13f9b6f37
uo-puddles <59064719+uo-puddles@users.noreply.github.com>
2021-02-08 14:58:15 -0800

    Update s21_puddles.py

--- s21_puddles.py
+++ s21_puddles.py
@@ -28,3 +28,47 @@ def knn(table, target_list):
 
   sorted_record = sorted(distance_record)
   return sorted_record
+
+def get_knn_winner(expert_list):
+  #[ [d,choice], ... ]
+  counts = {}
+  for d,c in expert_list:
+    if c not in {}:
+      counts[c] = 0
+    counts[c] += 1
+    
+  m = max(counts.values())
+  j = counts.values().find(m)
+  return counts.keys()[j]
+
+
+def knn_accuracy(training_table, testing_table, k):
+  training_choices = training_table[training_table.columns[-1]].unique().tolist()
+  testing_choices = testing_table[testing_table.columns[-1]].unique().tolist()
+  choices = list(set(training_choices + testing_choices))
+  n = len(testing_table)
+  record = []
+  correct = 0
+  for i in range(n):
+    test_row = testing_table.loc[i].to_list()
+    choice = test_row[-1]
+    number_list = test_row[:-1]
+    result = knn(training_table, number_list, k)
+    votes = [c for d,c in result]
+    vote_counts = []
+    
+    for c in choices:
+      count = votes.count(c)
+      vote_counts += [count]
+
+    m = max(vote_counts)
+    j = vote_counts.index(m)
+    winner = choices[j]
+    if winner == choice:
+      correct += 1
+    record += [(winner, choice)]
+
+  heat_map(record, choices)
+
+  return correct/n
+
