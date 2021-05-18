b175a28f917ab2602349f07d0fff128cbb37814f
uo-puddles <59064719+uo-puddles@users.noreply.github.com>
2021-05-18 15:08:50 -0700

    Update s21_puddles.py

--- s21_puddles.py
+++ s21_puddles.py
@@ -622,3 +622,66 @@ values = raw.values
 data = series_to_supervised(values, 1, 2)
 '''
       
+# split a univariate sequence into samples
+def split_sequence(sequence, n_steps):
+	X, y = list(), list()
+	for i in range(len(sequence)):
+		# find the end of this pattern
+		end_ix = i + n_steps
+		# check if we are beyond the sequence
+		if end_ix > len(sequence)-1:
+			break
+		# gather input and output parts of the pattern
+		seq_x, seq_y = sequence[i:end_ix], sequence[end_ix]
+		X.append(seq_x)
+		y.append(seq_y)
+	return np.array(X), np.array(y)
+
+'''
+# define input sequence
+raw_seq = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+# choose a number of time steps
+n_steps = 3
+# split into samples
+X, y = split_sequence(raw_seq, n_steps)
+# reshape from [samples, timesteps] into [samples, timesteps, features]
+n_features = 1
+X = X.reshape((X.shape[0], X.shape[1], n_features))
+# define model
+model = Sequential()
+model.add(Conv1D(filters=64, kernel_size=2, activation='relu', input_shape=(n_steps, n_features)))
+'''
+
+# split a multivariate sequence into samples
+def split_sequences(sequences, n_steps):
+	X, y = list(), list()
+	for i in range(len(sequences)):
+		# find the end of this pattern
+		end_ix = i + n_steps
+		# check if we are beyond the dataset
+		if end_ix > len(sequences):
+			break
+		# gather input and output parts of the pattern
+		seq_x, seq_y = sequences[i:end_ix, :-1], sequences[end_ix-1, -1]
+		X.append(seq_x)
+		y.append(seq_y)
+	return np.array(X), np.array(y)
+'''
+# define input sequence
+in_seq1 = array([10, 20, 30, 40, 50, 60, 70, 80, 90])
+in_seq2 = array([15, 25, 35, 45, 55, 65, 75, 85, 95])
+out_seq = array([in_seq1[i]+in_seq2[i] for i in range(len(in_seq1))])
+# convert to [rows, columns] structure
+in_seq1 = in_seq1.reshape((len(in_seq1), 1))
+in_seq2 = in_seq2.reshape((len(in_seq2), 1))
+out_seq = out_seq.reshape((len(out_seq), 1))
+# horizontally stack columns
+dataset = hstack((in_seq1, in_seq2, out_seq))
+# choose a number of time steps
+n_steps = 3
+# convert into input/output
+X, y = split_sequences(dataset, n_steps)
+
+model = Sequential()
+model.add(Conv1D(filters=64, kernel_size=2, activation='relu', input_shape=(n_steps, n_features)))
+'''
