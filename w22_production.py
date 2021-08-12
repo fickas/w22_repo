d5ca3b35c461e61efdb83b98e9cd54654009823b
fickas <fickas@cs.uoregon.edu>
2021-08-12 10:02:21 -0700

    Update w22_production.py

--- w22_production.py
+++ w22_production.py
@@ -1,2 +1,7 @@
+#IMPORTANT: any libraries used must be imported here.
+#Code in this file cannot see "outside" to the notebook execution context
+
+import pandas as pd   #etc.
+
 def hello():
   return 'world!'
